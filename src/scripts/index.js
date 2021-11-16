acfa2a2cb435f1e143b068d6a15d6e38d5c24885
Mario <mariothedev@gmail.com>
2021-11-16 00:38:19 -0800

    Insert defer and async attributes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,18 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+const slideIn3DContent = _ => anime({
+  targets: '.portfolio',
+  duration: 800,
+  skewY: ['-10deg', '-10deg'],
+  translateX: ['-105vw', 0],
+  translateY: ['-105vh', '7vmax'],
+})
+
+const boxParent = document.querySelector('.cube--demo')
+
+
+boxParent.addEventListener('animationend', _ => {
+  boxParent.style.display = "none"
+  slideIn3DContent()
+})
