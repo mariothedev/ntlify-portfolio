9d1137a69a14fb08c8f5b444738666d22d5f3cc3
Mario <mariothedev@gmail.com>
2021-11-30 10:04:11 +0200

    Add new functionality to hello

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,19 @@
+const jdenticon = require("jdenticon");
+const express = require("express");
+
+
 exports.handler = async function(event, context) {
     // your server-side functionality
+
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(
+        jdenticon.toSvg(req.params.identifier, Number.parseInt(req.params.size, 10))
+    );
+
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
+        body: jdenticon.toSvg(req.params.identifier, Number.parseInt(req.params.size, 10))
+    }
+
 }
